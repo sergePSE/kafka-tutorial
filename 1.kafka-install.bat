69c3cb399d91e581f7a4d67ea5ca82ecab210cad
Sergey Podanev <sergey.podanev@capgemini.com>
2021-03-09 16:30:25 +0100

    enumerated sources

--- 1.kafka-install.bat
+++ 1.kafka-install.bat
@@ -3,7 +3,7 @@ curl.exe --output C:\tmp\kafka.tgz https://downloads.apache.org/kafka/2.7.0/kafk
 cd C:\tmp
 tar -xf C:\tmp\kafka.tgz
 move C:\tmp\kafka_2.13-2.7.0 C:\tmp\kafka
-
+cd %script_dir%
 echo     Press any key to continue...
 pause >nul
 exit /b
