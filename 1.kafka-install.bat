f406e53191ce05c97a29cbad7115ccab486dad66
Sergey Podanev <sergey.podanev@capgemini.com>
2021-03-09 16:39:48 +0100

    delete old kafka if exists

--- 1.kafka-install.bat
+++ 1.kafka-install.bat
@@ -1,9 +1,10 @@
 set script_dir=%CD%
-curl.exe --output C:\tmp\kafka.tgz https://downloads.apache.org/kafka/2.7.0/kafka_2.13-2.7.0.tgz
+@REM curl.exe --output C:\tmp\kafka.tgz https://downloads.apache.org/kafka/2.7.0/kafka_2.13-2.7.0.tgz
 cd C:\tmp
+IF EXIST C:\tmp\kafka RMDIR /S /Q C:\tmp\kafka
 tar -xf C:\tmp\kafka.tgz
-move C:\tmp\kafka_2.13-2.7.0 C:\tmp\kafka
-cd %script_dir%
-echo     Press any key to continue...
-pause >nul
-exit /b
+@REM move C:\tmp\kafka_2.13-2.7.0 C:\tmp\kafka
+@REM cd %script_dir%
+@REM echo     Press any key to continue...
+@REM pause >nul
+@REM exit /b
