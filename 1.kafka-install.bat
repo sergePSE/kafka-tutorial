2f56cc4ab552f0792184fe47904ce74c0d8dc689
Sergey Podanev <sergey.podanev@capgemini.com>
2021-03-11 00:10:37 +0100

    aws script

--- 1.kafka-install.bat
+++ 1.kafka-install.bat
@@ -1,10 +1,9 @@
 set script_dir=%CD%
-@REM curl.exe --output C:\tmp\kafka.tgz https://downloads.apache.org/kafka/2.7.0/kafka_2.13-2.7.0.tgz
+curl.exe --output C:\tmp\kafka.tgz https://downloads.apache.org/kafka/2.7.0/kafka_2.13-2.7.0.tgz
 cd C:\tmp
 IF EXIST C:\tmp\kafka RMDIR /S /Q C:\tmp\kafka
 tar -xf C:\tmp\kafka.tgz
-@REM move C:\tmp\kafka_2.13-2.7.0 C:\tmp\kafka
-@REM cd %script_dir%
-@REM echo     Press any key to continue...
-@REM pause >nul
-@REM exit /b
+move C:\tmp\kafka_2.13-2.7.0 C:\tmp\kafka
+cd %script_dir%
+echo     Press any key to continue...
+pause >nul
---
+++ aws_server.sh
@@ -0,0 +1,16 @@
+sudo apt-get install openjdk-11-jdk
+sudo apt-get install maven
+curl --output kafka.tgz https://downloads.apache.org/kafka/2.7.0/kafka_2.13-2.7.0.tgz
+tar -xf kafka.tgz
+mv kafka_2.13-2.7.0 kafka
+cd kafka
+./bin/zookeeper-server-start.sh config/zookeeper.properties > /dev/null &
+sleep 30
+export KAFKA_HEAP_OPTS="-Xmx256M -Xms128M"
+./bin/kafka-server-start.sh config/server.properties > /dev/null &
+tar -xzf 3.27.0.tar.gz
+cd kafdrop-3.27.0
+mvn clean package
+java --add-opens=java.base/sun.nio.ch=ALL-UNNAMED -jar target/kafdrop-3.27.0.jar  --kafka.brokerConnect=localhost:9092
+wget https://github.com/obsidiandynamics/kafdrop/archive/3.27.0.tar.gz
+ 
